b5e2a7af3231cfde7441f4557ee5b0bc6811b2d6
unknown <spapanaidu@SPAPANAIDU01-LT.vocera.local>
2015-07-23 11:39:19 -0700

    Initial Push from IDE

--- ebs.py
+++ ebs.py
@@ -7,17 +7,16 @@ magnetic = 0
 total = 0
 io1 = 0
 
-vols = conn.get_all_volumes(filters={'status':'available'})
+vols = conn.get_all_volumes(filters={'status': 'available'})
 for vol in vols:
-    print 'checking vol:', vol.id, vol.status, 'type:',vol.type
+    print 'checking vol:', vol.id, vol.status, 'type:', vol.type
     total = total + vol.size
     if vol.type == 'gp2':
-        gp2 = gp2+vol.size
+        gp2 = gp2 + vol.size
     elif vol.type == 'standard':
         magnetic = magnetic + vol.size
     elif vol.type == 'io1':
         io1 = io1 + vol.size
-
 
 print 'total volume size:', total, 'GB'
 print 'gp2 volume size:', gp2, 'GB'
