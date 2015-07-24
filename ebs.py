ac3848964e0a528a1aadeaad64637f20d9ee524f
Shravan Papanaidu <shravanpn7@gmail.com>
2015-07-24 16:01:41 -0700

    Delete orphan EBS volumes functionality added

--- ebs.py
+++ ebs.py
@@ -1,4 +1,6 @@
 __author__ = 'spapanaidu'
+import sys
+
 from boto import ec2
 
 conn = ec2.connect_to_region('us-east-1')
@@ -6,10 +8,18 @@ gp2 = 0
 magnetic = 0
 total = 0
 io1 = 0
+volToDelete = []
 
 vols = conn.get_all_volumes(filters={'status': 'available'})
+
+if len(sys.argv) > 1 and sys.argv[1] == 'delete':
+    deleteVols = True
+else:
+    deleteVols = False
+
 for vol in vols:
     print 'checking vol:', vol.id, vol.status, 'type:', vol.type
+    volToDelete.append(str(vol.id))
     total = total + vol.size
     if vol.type == 'gp2':
         gp2 = gp2 + vol.size
@@ -22,4 +32,12 @@ print 'total volume size:', total, 'GB'
 print 'gp2 volume size:', gp2, 'GB'
 print 'magnetic volume size:', magnetic, 'GB'
 print 'io volume size:', io1, 'GB'
-##
+
+if deleteVols == True:
+    deleteConfirm = raw_input("Are you sure you want to delete the above orphan volumes [y/n]? ")
+    if deleteConfirm == 'y':
+        for vols in volToDelete:
+            print "Deleting " + vols
+            conn.delete_volume(vols)
+
+conn.close()
